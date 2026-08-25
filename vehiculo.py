83a27ff38d1118ef71e97e498cd79aaec7e318af
Michael Alexis Arjel Mayerovich <mike@MacBook-Pro-de-Michael.local>
2026-08-25 19:17:55 -0400

    Dejar vehiculo.py vacio

--- vehiculo.py
+++
@@ -1,8 +0,0 @@
-class Vehiculo:
-    def __init__(self, marca, modelo, año):
-        self.marca = marca
-        self.modelo = modelo
-        self.año = año
-
-    def __str__(self):
-        return f"{self.marca} {self.modelo} ({self.año})"
