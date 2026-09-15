a35f4df75e511bfb5bb535c0f1bdd3230fe12c05
Michael Alexis Arjel Mayerovich <mike@MacBook-Pro-de-Michael.local>
2026-09-15 18:58:37 -0300

    Agrega script conectar.py e ignora archivos .db

---
+++ conectar.py
@@ -0,0 +1,30 @@
+import sqlite3
+from marca import Marca
+from modelo import Modelo
+from auto import Auto
+
+
+conexion = sqlite3.connect("taller.db")
+
+cursor = conexion.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Vehiculo(
+             patente TEXT PRIMARY KEY,
+             modelo TEXT,
+             en_taller INTEGER)""")
+marca = Marca("Toyota")
+modelo = Modelo("Yaris", marca)
+
+auto = Auto("AB1234", 2027, modelo)
+
+#cursor.execute("INSERT INTO Vehiculo (patente, modelo, en_taller) VALUES (?,?,?)",
+#               (auto.patente, auto.modelo.nombre, int(auto._en_taller)))
+
+cursor.execute("Select * from Vehiculo where patente = ?",("AB1234",))
+fila= cursor.fetchone()
+print(fila)
+
+cursor.execute()
+
+conexion.commit()
+
